2be95ed9c6c17bfe8035cb0aa2feacb035fb670c
kent1 <kent1@arscenic.info>
2014-06-15 15:12:32 +0200

    Afficher le contenu du champ hal

--- hal_auteurs_pipelines.php
+++ hal_auteurs_pipelines.php
@@ -13,7 +13,8 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
  * Insertion dans le pipeline editer_contenu_objet (SPIP)
  * 
  * Ajout dans le formulaire d'édition de document du sélecteur de licence
- *
+ * 
+ * @pipeline editer_contenu_obje
  * @param array $flux 
  * 		Le contexte du pipeline
  * @return array  $flux 
@@ -25,6 +26,22 @@ function hal_auteurs_editer_contenu_objet($flux){
 			$ajouts = recuperer_fond('inclure/saisie_hal_auteurs',$flux['args']['contexte']);
 			$flux['data'] = str_replace($regs[0],$regs[0].$ajouts,$flux['data']);
 		}
+	}
+	return $flux;
+}
+
+/**
+ * Ajoute le champ hal sur la visualisation de l'auteur
+ * 
+ * @pipeline afficher_contenu_objet
+ * @param array $flux 
+ * 		Données du pipeline
+ * @return array $flux
+ *		Données du pipeline modifiées
+ */ 
+function hal_auteurs_afficher_contenu_objet($flux){
+	if($flux['args']['type'] == "auteur"){
+		$flux['data'] .= recuperer_fond('inclure/vue_hal_auteur',$flux['args']['contexte']);
 	}
 	return $flux;
 }
