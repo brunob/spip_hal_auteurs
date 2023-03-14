7ab7fcc34ab098ddcf15e07ff1f72d265244dded
brunob <bruno@eliaz.fr>
2023-03-14 19:29:28 +0100

    style: codings standards SCS1

--- base/hal_auteurs.php
+++ base/hal_auteurs.php
@@ -1,28 +1,32 @@
 <?php
+
 /**
  * Plugin HAL Auteurs
  * (c) 2014 kent1
  * Distribue sous licence GPL
- * 
+ *
  * Déclaration du champ hal supplémentaire sur les auteurs
- * 
+ *
  * @package SPIP\HAL_Auteurs\Pipelines
  */
-if (!defined('_ECRIRE_INC_VERSION')) return;
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
 
 /**
  * Insertion dans le pipeline declarer_tables_objets_sql (SPIP)
- * 
+ *
  * On ajoute le champ hal à la table spip_auteurs
- * 
+ *
  * @param array $tables
  * 		Le tableau des objets déclarés
  * @return array $tables
  * 		Le tableau des objets déclarés complété
  */
-function hal_auteurs_declarer_tables_objets_sql($tables){
+function hal_auteurs_declarer_tables_objets_sql($tables) {
 	$tables['spip_auteurs']['field']['hal'] = "text NOT NULL DEFAULT ''";
-	$tables['spip_auteurs']['champs_editables'][] = "hal";
-	$tables['spip_auteurs']['champs_versionnes'][] = "hal";
+	$tables['spip_auteurs']['champs_editables'][] = 'hal';
+	$tables['spip_auteurs']['champs_versionnes'][] = 'hal';
 	return $tables;
 }
